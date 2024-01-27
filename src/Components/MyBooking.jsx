a9f5f81cf3742750027cd7e9a7c097733dcb05bb
pratikshajain27 <pratiksha.jain@gravitonweb.com>
2024-01-27 23:23:51 +0530

    update

--- src/Components/MyBooking.jsx
+++ src/Components/MyBooking.jsx
@@ -100,7 +100,7 @@ const MyBooking = () => {
           <div className="min-h-screen flex items-center justify-center">
             <div className="current-data bg-white bg-opacity-10 shadow rounded p-5 w-full mx-5 md:mx-14 lg:mx-20 xl:mx-28">
               <div className="flex justify-between">
-                <h2 className="text-2xl font-bold mb-4 text-[#FFC61A]">
+                <h2 className="text-2xl font-bold text-[#FFC61A]">
                   Your Booked
                 </h2>
                 <div className="hidden md:block">
@@ -117,177 +117,173 @@ const MyBooking = () => {
                     Call US
                   </button>
                 </div>
-
-                
               </div>
 
-              <div className="block md:hidden">
-                  <p>
-                    <button
-                      className="bg-[#25d366] w-full text-white px-4 py-2 font-semibold rounded-xl"
-                      onClick={handleWhatsappChat}
-                    >
-                      Chat With Whatsapp
-                    </button>
-                  </p>
+              <div className="block md:hidden mt-4">
+                <p>
+                  <button
+                    className="bg-[#25d366] w-full text-white px-4 py-2 font-semibold rounded-xl"
+                    onClick={handleWhatsappChat}
+                  >
+                    Chat With Whatsapp
+                  </button>
+                </p>
 
-                  <p className="mt-4">
-                    <button
-                      className="bg-blue-400 w-full text-white px-4 py-2 font-semibold rounded-xl"
-                      onClick={handleCall}
-                    >
-                      Call US
-                    </button>
-                  </p>
+                <p className="mt-4">
+                  <button
+                    className="bg-blue-400 w-full text-white px-4 py-2 font-semibold rounded-xl"
+                    onClick={handleCall}
+                  >
+                    Call US
+                  </button>
+                </p>
               </div>
-
 
               <div className="overflow-x-auto ">
-
-              <table className="min-w-full mt-5">
-                <thead>
-                  <tr className="text-white">
-                    <th className="border p-4">Booking Id</th>
-                    <th className="border p-4">From Address</th>
-                    <th className="border p-4">To Address</th>
-                    <th className="border p-4">Date</th>
-                    <th className="border p-4">Time</th>
-                    <th className="border p-4">Phone</th>
-                    <th className="border p-4">Name</th>
-                    <th className="border p-4">Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {filteredData.map((dataItem, index) => (
-                    <tr key={index} className="text-white">
-                      <td className="border p-4">{dataItem.bookingId}</td>
-                      <td className="border p-4">{dataItem.fromaddress}</td>
-                      <td className="border p-4">{dataItem.toaddress}</td>
-                      <td className="border p-4">{dataItem.date}</td>
-                      <td className="border p-4">{dataItem.time}</td>
-                      <td className="border p-4">{dataItem.phone}</td>
-                      <td className="border p-4">{dataItem.name}</td>
-                      {dataItem.action == "active" ? (
-                        <td
-                          className="border bg-yellow-600 text-white font-bold rounded-xl p-2"
-                          onClick={GetTaxi}
-                        >
-                          Check Your Taxi
-                        </td>
-                      ) : dataItem.action == "pending" ? (
-                        <td className="border">
-                          <div className="flex justify-center">
-                            <span className="cursor-pointer bg-red-600 text-white font-bold rounded mx-5 xl:mx-0 px-4 py-1">
-                              Pending
-                            </span>
-                          </div>
-                        </td>
-                      ) : (
-                        <td className="border">
-                          <div className="flex justify-center">
-                            <span className="cursor-pointer bg-red-600 text-white font-bold rounded mx-5 xl:mx-0 px-4 py-1">
-                              Completed
-                            </span>
-                          </div>
-                        </td>
-                      )}
-                      {/* <td className="border bg-red-600 text-white font-bold rounded-xl p-2">Pending</td> */}
+                <table className="min-w-full mt-5">
+                  <thead>
+                    <tr className="text-white">
+                      <th className="border p-4">Booking Id</th>
+                      <th className="border p-4">From Address</th>
+                      <th className="border p-4">To Address</th>
+                      <th className="border p-4">Date</th>
+                      <th className="border p-4">Time</th>
+                      <th className="border p-4">Phone</th>
+                      <th className="border p-4">Name</th>
+                      <th className="border p-4">Status</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-
+                  </thead>
+                  <tbody>
+                    {filteredData.map((dataItem, index) => (
+                      <tr key={index} className="text-white">
+                        <td className="border p-4">{dataItem.bookingId}</td>
+                        <td className="border p-4">{dataItem.fromaddress}</td>
+                        <td className="border p-4">{dataItem.toaddress}</td>
+                        <td className="border p-4">{dataItem.date}</td>
+                        <td className="border p-4">{dataItem.time}</td>
+                        <td className="border p-4">{dataItem.phone}</td>
+                        <td className="border p-4">{dataItem.name}</td>
+                        {dataItem.action == "active" ? (
+                          <td className="border" onClick={GetTaxi}>
+                            <div className="flex justify-center">
+                              <span className="cursor-pointer bg-yellow-400 text-white font-bold rounded m-5 xl:mx-0 px-4 py-1">
+                                Check Your Taxi
+                              </span>
+                            </div>
+                          </td>
+                        ) : dataItem.action == "pending" ? (
+                          <td className="border">
+                            <div className="flex justify-center">
+                              <span className="cursor-pointer bg-red-600 text-white font-bold rounded mx-5 xl:mx-0 px-4 py-1">
+                                Pending
+                              </span>
+                            </div>
+                          </td>
+                        ) : (
+                          <td className="border">
+                            <div className="flex justify-center">
+                              <span className="cursor-pointer bg-red-600 text-white font-bold rounded mx-5 xl:mx-0 px-4 py-1">
+                                Completed
+                              </span>
+                            </div>
+                          </td>
+                        )}
+                        {/* <td className="border bg-red-600 text-white font-bold rounded-xl p-2">Pending</td> */}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
-              
             </div>
           </div>
         ) : (
-          <div className="relative">
-            <div className="flex justify-center items-center min-h-screen bg-blue-50">
-              <div className="max-w-4xl p-8 bg-white rounded shadow-md relative">
-                <Link
-                  onClick={BackHandle}
-                  className="absolute top-0 left-0 text-blue-500"
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="current-data bg-white bg-opacity-10 shadow rounded p-5 w-full mx-5 md:mx-14 lg:mx-20 xl:mx-28">
+              <Link onClick={BackHandle} className="text-white">
+                {/* Back Icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6"
                 >
-                  {/* Back Icon */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M15 19l-7-7 7-7"
-                    />
-                  </svg>
-                </Link>
-                <div className="flex justify-between mb-4 ">
-                  <h2 className="text-2xl font-bold mb-4 text-left text-gray-800">
-                    Your Booking Detail
-                  </h2>
-                  <div>
-                    <button
-                      onClick={handleCall}
-                      className="border bg-green-600 text-white font-semibold rounded-xl p-2"
-                    >
-                      Call Us
-                    </button>
-                    <button
-                      onClick={handleWhatsappChat}
-                      className="border bg-green-600 text-white font-semibold rounded-xl p-2"
-                    >
-                      {" "}
-                      Chat With WhatsApp
-                    </button>
-                  </div>
-                </div>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 19l-7-7 7-7"
+                  />
+                </svg>
+              </Link>
 
-                <table className="w-full border-collapse">
+              <div className="flex justify-between mt-4">
+                <h2 className="text-2xl font-bold  text-[#FFC61A]">
+                  Your Booking Detail
+                </h2>
+                <div className="hidden md:block">
+                  <button
+                    className="bg-[#25d366] text-white px-4 py-2 font-semibold mr-4 rounded-xl"
+                    onClick={handleWhatsappChat}
+                  >
+                    Chat With Whatsapp
+                  </button>
+                  <button
+                    className="bg-blue-400 text-white px-4 py-2 font-semibold rounded-xl"
+                    onClick={handleCall}
+                  >
+                    Call US
+                  </button>
+                </div>
+              </div>
+
+              <div className="block md:hidden mt-4">
+                <p>
+                  <button
+                    className="bg-[#25d366] w-full text-white px-4 py-2 font-semibold rounded-xl"
+                    onClick={handleWhatsappChat}
+                  >
+                    Chat With Whatsapp
+                  </button>
+                </p>
+
+                <p className="mt-4">
+                  <button
+                    className="bg-blue-400 w-full text-white px-4 py-2 font-semibold rounded-xl"
+                    onClick={handleCall}
+                  >
+                    Call US
+                  </button>
+                </p>
+              </div>
+
+              <div className="overflow-x-auto ">
+                <table className="min-w-full mt-5">
                   <thead>
-                    <tr className="bg-blue-100">
-                      <th className="py-2 px-4 border">BookindId</th>
-                      <th className="py-2 px-4 border">Name</th>
-                      <th className="py-2 px-4 border">Address</th>
-                      <th className="py-2 px-4 border">Vehicle Model</th>
-                      <th className="py-2 px-4 border">Vehicle Number</th>
-                      <th className="py-2 px-4 border">Seat Capacity</th>
-                      <th className="py-2 px-4 border">Total Amount</th>
-                      <th className="py-2 px-4 border">Amount Per Km</th>
-                      <th className="py-2 px-4 border">Sell Amount</th>
+                    <tr className="text-white">
+                      <th className="p-4 border">Booking Id</th>
+                      <th className="p-4 border">Name</th>
+                      <th className="p-4 border">Address</th>
+                      <th className="p-4 border">Vehicle Model</th>
+                      <th className="p-4 border">Vehicle Number</th>
+                      <th className="p-4 border">Seat Capacity</th>
+                      <th className="p-4 border">Total Amount</th>
+                      <th className="p-4 border">Amount Per Km</th>
+                      <th className="p-4 border">Sell Amount</th>
                     </tr>
                   </thead>
                   <tbody>
                     {driverData.map((driver) => (
-                      <tr key={driver.DriverId} className="bg-blue-50">
-                        <td className="py-2 px-4 border">{driver.bookingId}</td>
-                        <td className="py-2 px-4 border">
-                          {driver.DriverName}
-                        </td>
-                        <td className="py-2 px-4 border">
-                          {driver.DriverAddress}
-                        </td>
-                        <td className="py-2 px-4 border">
-                          {driver.VehicleModel}
-                        </td>
-                        <td className="py-2 px-4 border">
-                          {driver.VehicleNumber}
-                        </td>
-                        <td className="py-2 px-4 border">
-                          {driver.SeatCapacity}
-                        </td>
-                        <td className="py-2 px-4 border">
-                          {driver.TotalAmount}
-                        </td>
-                        <td className="py-2 px-4 border">
-                          {driver.AmountPerKm}
-                        </td>
-                        <td className="py-2 px-4 border">
-                          {driver.SellAmount}
-                        </td>
+                      <tr key={driver.DriverId} className="text-white">
+                        <td className="p-4 border">{driver.bookingId}</td>
+                        <td className="p-4 border">{driver.DriverName}</td>
+                        <td className="p-4 border">{driver.DriverAddress}</td>
+                        <td className="p-4 border">{driver.VehicleModel}</td>
+                        <td className="p-4 border">{driver.VehicleNumber}</td>
+                        <td className="p-4 border">{driver.SeatCapacity}</td>
+                        <td className="p-4 border">{driver.TotalAmount}</td>
+                        <td className="p-4 border">{driver.AmountPerKm}</td>
+                        <td className="p-4 border">{driver.SellAmount}</td>
                       </tr>
                     ))}
                   </tbody>
